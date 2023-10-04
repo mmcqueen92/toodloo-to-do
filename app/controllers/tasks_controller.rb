1fe33777ed4a6db2fc8c554353a73b1f873589d3
Malcolm McQueen <110631738+mmcqueen92@users.noreply.github.com>
2023-10-04 03:14:32 -0700

    create first batch of routes and tasks controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,50 +1,111 @@
 class TasksController < ApplicationController
-    before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
   
-    def index
-      @tasks = Task.all
+  def index
+    @tasks = Task.all
+  end
+
+  # gets all of user's tasks
+  def index_for_user
+    @tasks = current_user.tasks
+  end
+
+  # show a specific task
+  def show_for_user
+    @task = current_user.tasks.find(params[:id])
+  end
+
+  # show users tasks for the day
+  def today_tasks_for_user
+    @today_tasks = current_user.tasks.where(due_date: Date.today)
+  end
+
+  # builds a new task from user input in form
+  def new_for_user
+    @task = current_user.tasks.build
+  end
+  
+  # creates a new task for user
+  def create_for_user
+    @task = current_user.tasks.build(task_params)
+    if @task.save
+      redirect_to @task, notice: 'Task was successfully created.'
+    else
+      render :new_for_user
     end
-  
-    def show
+  end
+
+  # populates form with current task info
+  def edit_for_user
+    @task = current_user.tasks.find(params[:id])
+  end
+
+  # updates a task
+  def update
+    @task = current_user.find(params[:id])
+    if @task.update(task_params)
+      redirect_to @task, notice: 'Task was successfully updated.'
+    else
+      render :edit_for_user
     end
+  end
   
-    def new
-      @task = Task.new
+  # mark a task complete
+  def complete
+    @task = current_user.tasks.find(params[:id])
+    @task.update(completed: true)
+    redirect_to tasks_url, notice: 'Task marked as complete.'
+  end
+
+  # mark a task incomplete
+  def incomplete
+    @task = current_user.tasks.find(params[:id])
+    @task.update(completed: false)
+    redirect_to tasks_url, notice: 'Task marked as incomplete.'
+  end
+
+  # delete a task
+  def destroy
+    @task = current_user.tasks.find(params[:id])
+    @task.destroy
+    redirect_to tasks_url, notice: 'Task was successfully destroyed.'
+  end
+
+  # gets all of a user's completed tasks within a range of dates
+  def completed_tasks_by_date_for_user
+    if params[:start_date].present? && params[:end_date].present?
+      start_date = Date.parse(params[:start_date])
+      end_date = Date.parse(params[:end_date])
+      @completed_tasks = current_user.tasks.where(completed: true, due_date: start_date..end_date)
+    else
+      @completed_tasks = current_user.tasks.where(completed: true)
     end
-  
-    def create
-      @task = Task.new(task_params)
-      if @task.save
-        redirect_to @task, notice: 'Task was successfully created.'
-      else
-        render :new
-      end
+  end
+
+
+  # gets all of a user's incomplete tasks within a date range
+  def incomplete_tasks_by_date_for_user
+    if params[:start_date].present? && params[:end_date].present?
+      start_date = Date.parse(params[:start_date])
+      end_date = Date.parse(params[:end_date])
+      @incomplete_tasks = current_user.tasks.where(completed: false, due_date: start_date..end_date)
+    else
+      @incomplete_tasks = current_user.tasks.where(completed: false)
     end
+  end
+
   
-    def edit
-    end
   
-    def update
-      if @task.update(task_params)
-        redirect_to @task, notice: 'Task was successfully updated.'
-      else
-        render :edit
-      end
-    end
+  private
   
-    def destroy
-      @task.destroy
-      redirect_to tasks_url, notice: 'Task was successfully destroyed.'
-    end
+  def set_task
+    @task = Task.find(params[:id])
+  end
   
-    private
-  
-    def set_task
-      @task = Task.find(params[:id])
-    end
-  
-    def task_params
-      params.require(:task).permit(:title, :description, :due_date, :completed)
-    end
+  def task_params
+    params.require(:task).permit(:title, :description, :due_date, :completed)
+  end
+
+
   end
   
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,48 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources :tasks
   # Defines the root path route ("/")
   # root "articles#index"
+  devise_for :users, controllers: { registrations: 'registrations' }
+  devise_for :users, controllers: { sessions: 'sessions' }
+  devise_for :users, controllers: { sessions: 'sessions' } do
+    delete 'logout', to: 'sessions#destroy', as: :destroy_user_session
+  end
+
+  # mark a task complete
+  patch '/tasks/:id/complete', to: 'tasks#complete', as: 'complete_task'
+
+  # mark a task incomplete
+  patch '/tasks/:id/incomplete', to: 'tasks#incomplete', as: 'incomplete_task'
+
+  # render a form for a new task belonging to a user
+  get '/tasks/new', to: 'tasks#new_for_user', as: 'new_user_task'
+  
+  # create a new task belonging to a user
+  post '/tasks', to: 'tasks#create_for_user', as: 'create_user_task'
+  
+  # get all of a user's tasks
+  get '/my_tasks', to: 'tasks#index_for_user', as: 'user_tasks'
+
+  # get all of a user's tasks due today
+  get '/today_tasks', to: 'tasks#today_for_user', as: 'today_tasks_for_user'
+  
+  # get one of a user's tasks
+  get '/my_tasks/:id', to: 'tasks#show_for_user', as: 'user_task'
+  
+  # get one of a user's tasks to edit
+  get '/my_tasks/:id/edit', to: 'tasks#edit_for_user', as: 'edit_user_task'
+  
+  # save the edited task
+  patch '/my_tasks/:id', to: 'tasks#update_for_user', as: 'update_user_task'
+  
+  # delete a task
+  delete '/my_tasks/:id', to: 'tasks#destroy_for_user', as: 'destroy_user_task'
+
+  # search for all of a user's completed tasks by range of dates
+  get '/completed_tasks/:start_date/:end_date', to: 'tasks#completed_for_user', as: 'completed_tasks_by_date_for_user'
+
+  # search for all of a user's incomplete tasks by range of dates
+  get '/incomplete_tasks/:start_date/:end_date', to: 'tasks#incomplete_tasks_for_user', as: 'incomplete_tasks_by_date_for_user'
+
 end
