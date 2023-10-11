0d62ebb380758bdcefccaed10628e8e023f3ae76
Malcolm McQueen <110631738+mmcqueen92@users.noreply.github.com>
2023-10-10 21:24:01 -0700

    scss

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,3 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-import { dashboard } from "./dashboard";
-
-console.log("RUNNING APPLICATION.JS")
